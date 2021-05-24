68d4350159008a8412c6205977f3d56932ecd87f
Octavian <a1521726@bangkit.academy>
2021-05-24 07:17:46 +0700

    Add HomePage + UI Structure

---
+++ BansosAppAndroid/app/src/main/java/com/octatech/bansosapp/ui/SplashScreenPage.kt
@@ -0,0 +1,12 @@
+package com.octatech.bansosapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.octatech.bansosapp.R
+
+class SplashScreenPage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
